b0862eeddc359318944bf2698a2c8e95b77ba30b
aliang970330 <3466396665@qq.com>
2020-09-03 14:41:00 +0800

    indexover

--- assets/js/baseAPI.js
+++ assets/js/baseAPI.js
@@ -4,4 +4,27 @@
 $.ajaxPrefilter(function (options) {
     // 在发起真正的ajax请求之前 统一拼接请求的根路径
     options.url = 'http://ajax.frontend.itheima.net' + options.url
+
+
+
+    // 统一为有权限的借口设置 headers 设置请求头
+    if (options.url.indexOf('/my/') !== -1) {
+        options.headers = {
+            Authorization: localStorage.getItem('token') || ''
+        }
+    }
+
+
+    // 设置访问权限
+    options.complete = function (res) {
+        // 在complete回调函数中可以拿到
+
+        // res.responseJSON拿到服务器响应回来的数据
+        if (res.responseJSON.status === 1 && res.responseJSON.message === "身份认证失败！") {
+            //清空token
+            localStorage.removeItem('token')
+            //强制跳转到登录页面
+            location.href = '/login.html'
+        }
+    }
 })
